6326072c405492ea1a03ecd48d92db46de1b9dfd
Joey Kraut <josephk@berkeley.edu>
2020-05-20 09:55:07 -0700

    Added initial image encoder structure

--- model/model.py
+++ model/model.py
@@ -30,8 +30,7 @@ class ResidualBlock(nn.Module):
         self.downsample_after = downsample_after
 
         # Define the structure of the residual block
-        padding_amount = get_padding_size(input_size, stride, kernel_size)
-        print(f"Needed padding: {padding_amount}")
+        padding_amount = int(get_padding_size(input_size, stride, kernel_size))
 
         # Define the convolution layers
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding_amount)
@@ -69,3 +68,36 @@ class ResidualBlock(nn.Module):
             return (self.input_size // 2) ** 2 * self.out_channels
         else:
             return self.input_size ** 2 * self.out_channels
+
+class ImageEncoder(nn.Module):
+    def __init__(self, input_size, latent_dim, num_blocks=4):
+        """
+        Initializes the network that serves as an encoder from Image Input --> Latent Space
+        """
+        super(ImageEncoder, self).__init__()
+
+        # Build 4 residual blocks to downsample
+        input_sizes = [input_size * 2 ** (-1 * i) for i in range(1, num_blocks)]
+        self.init_block = ResidualBlock(input_size, 3, 128, 3)
+
+        self.res_blocks = [ResidualBlock(in_size, 128, 128, 3) for in_size in input_sizes]
+
+        final_input_size = int(input_size * (2 ** (-num_blocks)))
+        self.fully_connected = nn.Linear(128 * (final_input_size) ** 2, latent_dim)
+
+    def forward(self, x):
+        """
+        Performs the model forward pass
+        :param x: The model input
+        :return: The model output
+        """
+        x = self.init_block(x)
+
+        for res_block in self.res_blocks:
+            x = res_block(x)
+
+        x = x.view(x.size()[0], -1)
+
+        return F.relu(self.fully_connected(x))
+
+
