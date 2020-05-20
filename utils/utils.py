04d380f67c811504d566f5f5237feeb768ac4600
Joey Kraut <josephk@berkeley.edu>
2020-05-20 10:54:20 -0700

    Changed API for write_graph to take in tuple of dims instead of tensor for dummy input

--- utils/utils.py
+++ utils/utils.py
@@ -4,14 +4,19 @@ This file defines a series of utils for training, testing, and debugging
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
-def write_graph(network, dummy_input):
+def write_graph(network, input_dims):
     """
     Writes the graph given by <network> to tensorboard for inspection
     :param network: The network we want to put on tensorboard
+    :param input_dims: The dimensions this network expects as input
     :return: None
     """
     # Build the torch writer
     writer = SummaryWriter("../logs/graphs")
+
+    # Get a dummy input, must be in this form for jit trace
+    dummy_input = (torch.zeros(input_dims))
+
     # Write to tensorboard
     writer.add_graph(network, input_to_model=dummy_input, verbose=False)
     writer.flush()
